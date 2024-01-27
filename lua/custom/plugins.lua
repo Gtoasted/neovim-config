7da538b6da9d1ce6d9c52cc6e1313059094cca55
Arne von Laguna <arne@vonlaguna.de>
2024-01-27 16:07:36 +0100

    Changed nvim-cmpl keys

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,5 @@
+local cmp = require "cmp"
+
 local viewer = ''
 if package.config:sub(1,1) == '/' then
   viewer = 'zathura'
@@ -6,8 +8,7 @@ else
 end
 
 return {
-	{
-    'lervag/vimtex',
+	{'lervag/vimtex',
     lazy = false,
     config = function(_)
       vim.g.tex_flavor='latex'
@@ -23,17 +24,38 @@ return {
         {name = 'vec', mathmode = true , conceal = true, argstyle = 'bold'}
       }
     end
-  },{
-    'L3MON4D3/LuaSnip',
+  },{'L3MON4D3/LuaSnip',
     opts = {enable_autosnippets = true}
-  },{
-    'rafamadriz/friendly-snippets',
+  },{'rafamadriz/friendly-snippets',
     enabled = false,
-  },{
-    'hrsh7th/nvim-cmp',
+  },{'hrsh7th/nvim-cmp',
     opts = {
-      completion = {
-        keyword_length = 3,
+      completion = {keyword_length = 3},
+      mapping = {
+        ["Up"] = cmp.mapping.select_prev_item(),
+        ["Down"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+        "i",
+        "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        })
       }
     }
   },{
